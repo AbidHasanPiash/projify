51a47bf703ef41d876b10c2cf705ccbca1c683f3
Abid <meghpiash@gmail.com>
2023-09-14 23:29:05 +0600

    timeline design

--- components/Projects/Task.jsx
+++ components/Projects/Task.jsx
@@ -1,29 +1,56 @@
 'use client'
 import React, { useEffect, useState } from 'react'
+import {Avatar, AvatarGroup, Tooltip} from "@nextui-org/react";
+import { HiPencilSquare } from "react-icons/hi2";
+import { HiPencil, HiTrash, HiBadgeCheck } from "react-icons/hi";
 
 export default function Task({task}) {
     const [bgMain, setBgMain] = useState('bg-orange-900');
-    const [bgDate, setBgDate] = useState('bg-orange-500');
+    const [bgSecond, setBgSecond] = useState('bg-orange-500');
     const [date, setDate] = useState(task.initiateDate);
     useEffect(() => {
         if (task.status == 'Reviewing') {
-            setBgMain('bg-cyan-900');
-            setBgDate('bg-cyan-500');
+            setBgMain('bg-violet-900');
+            setBgSecond('bg-violet-500');
             setDate(task.reviewDate);
         }
         else if (task.status == 'Complete') {
             setBgMain('bg-green-900');
-            setBgDate('bg-green-500');
+            setBgSecond('bg-green-500');
             setDate(task.completeDate);
         }
     }, [task.status])
     
   return (
     <div className={`${bgMain} p-2 rounded-lg bg-gray-900 border border-gray-700 text-gray-300`}>
-        <span className={`${bgDate} px-1 py-0.5 text-gray-950 rounded-full text-xs font-bold tracking-widest`}>{date}</span>
-        <h3 className='text-xl font-semibold tracking-wider mt-1 mb-2'>{task.title}</h3>
+        <div className='flex items-start justify-between'>
+            <div>
+                <span className={`${bgSecond} px-1 py-0.5 text-gray-950 rounded-full text-xs font-bold tracking-widest`}>{date}</span>
+                <h3 className='text-xl font-semibold tracking-wider mt-1 mb-2'>{task.title}</h3>
+            </div>
+            <div>
+                <span className='text-2xl font-mono'># {task.number}</span>
+            </div>
+        </div>
         <p>{task.discription}</p>
-        <p>{task.status}</p>
+        <div className='pt-2.5 flex items-center justify-between'>
+            <AvatarGroup isBordered className="dark px-3" size="sm">
+                {task.assigned?.map((t, i)=>(
+                    <Tooltip key={i} color={'success'} content={t.name}>
+                    <Avatar size="sm" src={t.src}/>
+                    </Tooltip>
+                ))}
+            </AvatarGroup>
+            <div className='flex space-x-3'>
+                <span className={`${bgSecond} py-0.5 px-3 text-gray-950 rounded-full flex items-end justify-end`}>
+                    <HiBadgeCheck/>
+                </span>
+                <div className={`${bgSecond} py-0.5 px-3 text-gray-950 rounded-full flex items-end justify-end space-x-4`}>
+                    <HiPencilSquare/>
+                    <HiTrash/>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
--- components/Projects/Timeline.jsx
+++ components/Projects/Timeline.jsx
@@ -1,57 +1,103 @@
 'use client'
 import React, { useState } from 'react'
 import Task from './Task';
+import { Button } from '@nextui-org/react';
+import { HiPlus } from 'react-icons/hi2';
 
 export default function Timeline({selectedProject}) {
     const [Tasks, setTasks] = useState([
         {
-            title: 'Task 1',
+            title: 'Title',
+            number: '1',
             project: 'RestoMan',
             initiateDate: '12 December 2023',
             assignTo:['Tom', 'Amy'],
             reviewDate: '12 December 2023',
             completeDate: '12 December 2023',
             status: 'Complete',
+            priority: 'Normal',
+            assigned:[
+                {name:'Tom', src:"https://i.pravatar.cc/150?u=a042581f4e29026024d"},
+              ],
             discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. molestias natus expedita in eveniet! Nemo error officia sed? Facere veritatis labore id totam.',
         },
         {
-            title: 'Task 2',
+            title: 'Title',
+            number: '2',
             project: 'RestoMan',
             initiateDate: '12 December 2023',
             assignTo:['Tom', 'Amy'],
             reviewDate: '12 December 2023',
             completeDate: '12 December 2023',
             status: 'Processing',
+            priority: 'High',
+            assigned:[
+                {name:'Tom', src:"https://i.pravatar.cc/150?u=a042581f4e29026024d"},
+                {name:'Lee', src:"https://i.pravatar.cc/150?u=a04258114e29026302d"},
+              ],
             discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. molestias natus expedita in eveniet! Nemo error officia sed? Facere veritatis labore id totam.',
         },
         {
-            title: 'Task 3',
+            title: 'Title',
+            number: '3',
             project: 'RestoMan',
             initiateDate: '12 December 2023',
             assignTo:['Tom', 'Amy'],
             reviewDate: '12 December 2023',
             completeDate: '12 December 2023',
             status: 'Reviewing',
+            priority: 'Medium',
+            assigned:[
+                {name:'Bob', src:"https://i.pravatar.cc/150?u=a042581f4e29026704d"},
+                {name:'Lee', src:"https://i.pravatar.cc/150?u=a04258114e29026302d"},
+              ],
             discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. molestias natus expedita in eveniet! Nemo error officia sed? Facere veritatis labore id totam.',
         },
         {
-            title: 'Task 4',
+            title: 'Title',
+            number: '4',
             project: 'RestoMan',
             initiateDate: '12 December 2023',
             assignTo:['Tom', 'Amy'],
             reviewDate: '12 December 2023',
             completeDate: '12 December 2023',
             status: 'Processing',
+            priority: 'High',
+            assigned:[
+                {name:'Lee', src:"https://i.pravatar.cc/150?u=a04258114e29026302d"},
+              ],
             discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. molestias natus expedita in eveniet! Nemo error officia sed? Facere veritatis labore id totam.',
         },
         {
-            title: 'Task 5',
+            title: 'Title',
+            number: '5',
             project: 'RestoMan',
             initiateDate: '12 December 2023',
             assignTo:['Tom', 'Amy'],
             reviewDate: '12 December 2023',
             completeDate: '12 December 2023',
             status: 'Processing',
+            priority: 'Medium',
+            assigned:[
+                {name:'Bob', src:"https://i.pravatar.cc/150?u=a042581f4e29026704d"},
+                {name:'Lee', src:"https://i.pravatar.cc/150?u=a04258114e29026302d"},
+              ],
+            discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. molestias natus expedita in eveniet! Nemo error officia sed? Facere veritatis labore id totam.',
+        },
+        {
+            title: 'Title',
+            number: '6',
+            project: 'RestoMan',
+            initiateDate: '12 December 2023',
+            assignTo:['Tom', 'Amy'],
+            reviewDate: '12 December 2023',
+            completeDate: '12 December 2023',
+            status: 'Processing',
+            priority: 'Normal',
+            assigned:[
+                {name:'Tom', src:"https://i.pravatar.cc/150?u=a042581f4e29026024d"},
+                {name:'Lee', src:"https://i.pravatar.cc/150?u=a04258114e29026302d"},
+              ],
             discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. molestias natus expedita in eveniet! Nemo error officia sed? Facere veritatis labore id totam.',
         },
     ]);
@@ -62,8 +108,14 @@ export default function Timeline({selectedProject}) {
     <div className='pb-3 md:pb-6'>
         <div className='grid md:grid-cols-3 gap-3 md:gap-6'>
             <section className='h-fit rounded-lg bg-gray-900'>
-                <div className='sticky top-0 mb-3 py-3 z-10 bg-gray-900'>
-                    <h2 className='text-2xl text-gray-400 px-3 py-4 font-bold tracking-widest'>Processing</h2>
+                <div className='sticky top-0 mb-3 py-3 z-10 rounded-lg bg-gray-900 flex items-center justify-between'>
+                    <h2 className='text-2xl text-gray-400 px-3 py-4 font-bold tracking-widest'>
+                        <span>Processing </span>
+                        <span> {processingTasks.length}</span>
+                    </h2>
+                    <Button isIconOnly color="danger" aria-label="Like" variant='transparent' className='p-0 m-0 hover:text-orange-500 rounded-full cursor-pointer flex items-center justify-center hover:bg-gray-700'>
+                        <HiPlus/>
+                    </Button>
                 </div>
                 {/* Timeline */}
                 {processingTasks.map((task, i) => (
@@ -77,8 +129,11 @@ export default function Timeline({selectedProject}) {
                 ))}
             </section>
             <section className='h-fit rounded-lg bg-gray-900'>
-                <div className='sticky top-0 mb-3 py-3 z-10 bg-gray-900'>
-                    <h2 className='text-2xl text-gray-400 px-3 py-4 font-bold tracking-widest'>Reviewing</h2>
+                <div className='sticky top-0 mb-3 py-3 z-10 rounded-lg bg-gray-900'>
+                    <h2 className='text-2xl text-gray-400 px-3 py-4 font-bold tracking-widest'>
+                        <span>Reviewing </span>
+                        <span> {reviewingTasks.length}</span>
+                    </h2>
                 </div>
                 {/* Timeline */}
                 {reviewingTasks.map((task, i) => (
@@ -88,8 +143,11 @@ export default function Timeline({selectedProject}) {
                 ))}
             </section>
             <section className='h-fit rounded-lg bg-gray-900'>
-                <div className='sticky top-0 mb-3 py-3 z-10 bg-gray-900'>
-                    <h2 className='text-2xl text-gray-400 px-3 py-4 font-bold tracking-widest'>Complete</h2>
+                <div className='sticky top-0 mb-3 py-3 z-10 rounded-lg bg-gray-900'>
+                    <h2 className='text-2xl text-gray-400 px-3 py-4 font-bold tracking-widest'>
+                        <span>Complete </span>
+                        <span> {completeTasks.length}</span>
+                    </h2>
                 </div>
                 {/* Timeline */}
                 {completeTasks.map((task, i) => (
